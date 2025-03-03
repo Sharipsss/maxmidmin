05914e5bf8e8250230d557023dd628199d68dc37
Tabek <aetsharips@gmail.com>
2025-03-03 23:10:31 +0500

    fix

--- maxmidmin/Program.cs
+++ maxmidmin/Program.cs
@@ -4,11 +4,33 @@ namespace MMM
 {
     class MaxMidMin
     {
-        public static void Main(string[] args)
+        public static void CountMaxMidMin(int[] numbers)
         {
-            int[] numbers = { 0, 0, 0 };
             int dval = numbers[0];
             int[] statuses = { dval, 0, 2147483647 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (numbers[i] >= statuses[0])
+                {
+                    statuses[0] = numbers[i];
+                }
+
+                if (numbers[i] <= statuses[2])
+                {
+                    statuses[2] = numbers[i];
+                }
+            }
+
+            statuses[1] = (numbers[0] + numbers[1] + numbers[2]) - (statuses[0] + statuses[2]);
+            Console.WriteLine("MAX: {0}\nMID: {1}\nMIN: {2}", statuses[0], statuses[1], statuses[2]);
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void Main (string[] args)
+        {
+            int[] numbers = { 0, 0, 0 };
             if (args.Length == 3)
             {
                 for (int n = 0; n < 3; n++)
@@ -16,21 +38,19 @@ namespace MMM
                     numbers[n] = Int32.Parse(args[n]);
                 }
 
+                CountMaxMidMin(numbers);
+            }
+            else
+            {
+                Console.WriteLine("Optimal usage: maxmidmin [integers a, b, c]");
+
                 for (int i = 0; i < 3; i++)
                 {
-                    if (numbers[i] >= statuses[0])
-                    {
-                        statuses[0] = numbers[i];
-                    }
-
-                    if (numbers[i] <= statuses[2])
-                    {
-                        statuses[2] = numbers[i];
-                    }
+                    Console.Write("Enter {0} value: ", i + 1);
+                    numbers[i] = Int32.Parse(Console.ReadLine());
                 }
 
-                statuses[1] = (numbers[0] + numbers[1] + numbers[2]) - (statuses[0] + statuses[2]);
-                Console.WriteLine("MAX: {0}\nMID: {1}\nMIN: {2}", statuses[0], statuses[1], statuses[2]);
+                CountMaxMidMin(numbers);
             }
         }
     }
